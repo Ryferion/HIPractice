5ad8876471c9a84546d697f2d6dfeacb2f14bd2d
Ryferion <42130642+Ryferion@users.noreply.github.com>
2022-11-15 11:06:09 -0800

    Update Host.cpp

--- Host.cpp
+++ Host.cpp
@@ -29,9 +29,9 @@ int main()
     int deviceCount = 0, deviceID = 0;
     // hipSetDevice(2); // use GPU 2x
     cout << " line: " << __LINE__ << endl;
-    HIP_CHECK(hipGetDevice(&deviceID));
+    hipGetDevice(&deviceID);
     cout << " line: " << __LINE__ << endl;
-    HIP_CHECK(hipGetDeviceCount(&deviceCount));
+    hipGetDeviceCount(&deviceCount);
     
     cout << " line: " << __LINE__ << " num devices: " << deviceCount << " current device ID: " << deviceID << endl;
     // cout << __LINE__ << endl;
