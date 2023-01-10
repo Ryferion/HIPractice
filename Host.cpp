8a0615e61aabc9a2dbb92189b707a39404921f7c
Ryferion <42130642+Ryferion@users.noreply.github.com>
2023-01-10 11:05:11 -0800

    Update Host.cpp

--- Host.cpp
+++ Host.cpp
@@ -181,7 +181,7 @@ int main(int argc, char **argv)
     
     // vector <uint32_t> CUMask;
     uint32_t CUMask[2];
-    const uint32_t CUMask_size = 8;
+    const uint32_t CUMask_size = 2;
 
     // for (int iter = 0; iter < mask; iter++)
     { 
@@ -270,7 +270,7 @@ int main(int argc, char **argv)
 
     HIP_CHECK(hipStreamSynchronize(streamMemory));
     
-    
+
     // copy matrix data from device to host
     HIP_CHECK(hipMemcpyAsync(C_host, C_device, sizeof(float) * C_size, hipMemcpyDeviceToHost, streamMemory)); // host waits for kernel to finish here since hipMemcpy is blocking
 
