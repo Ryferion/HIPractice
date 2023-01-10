203b458bc305ddc2ee5984d78326adb2cbaf68fa
Ryferion <42130642+Ryferion@users.noreply.github.com>
2023-01-09 16:23:55 -0800

    Update Host.cpp

--- Host.cpp
+++ Host.cpp
@@ -252,8 +252,8 @@ int main(int argc, char **argv)
     HIP_CHECK(hipMemcpyAsync(B_device, B_host, sizeof(float) * B_size, hipMemcpyHostToDevice, streamMemory));
 
     HIP_CHECK(hipExtStreamGetCUMask(streamMultiply, CUMask_size, CUMask));
-    cout << CUMask << endl;
-    cout << " CUMask: " << std::bitset<32 * 2>(CUMask) << endl;
+    // cout << CUMask << endl;
+    // cout << " CUMask: " << std::bitset<32 * 2>(CUMask) << endl;
     // set up block dim and thread dim
     dim3 blocks(col / TILE_SIZE + 1, row / TILE_SIZE + 1, 1); // 3D dimensions of the grid of blocks
     dim3 threads(TILE_SIZE, TILE_SIZE, 1); // 3D dimensions of a block of threads
