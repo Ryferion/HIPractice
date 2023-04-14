3f6a7713e7d2f2291854095b63f96c44582c291c
Ryferion <42130642+Ryferion@users.noreply.github.com>
2023-04-14 15:15:28 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-ROCM_PATH=/opt/rocm
-HIP_PATH=${ROCM_PATH}/hip
-ROCM_TOOLKIT_PATH=${ROCM_PATH}
-ROCPROFILER=${ROCM_PATH}/rocprofiler
-DEVICE_LIB_PATH=${ROCM_PATH}/rocdl/amdgcn/bitcode
-PATH=${ROCM_PATH}/llvm/bin:${ROCM_PATH}/bin:${HIP_PATH}/bin:$PATH
-LD_LIBRARY_PATH=${ROCM_PATH}/comgr/lib:${HIP_PATH}/lib:${ROCM_PATH}/rocrand/lib:${ROCM_PATH}/hiprand/lib:${ROCM_PATH}/lib:$LD_LIBRARY_PATH
-GFXLIST="gfx906"
+#!/bin/sh
+rm -fr build
+mkdir build
+cd build
+cmake ..
+make -j 1
