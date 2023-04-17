929995ce1df19ac0b0b4276c2e9378ec8debc70f
Ryan <rquac004@ucr.edu>
2023-04-17 09:49:11 -0700

    build wit hipcc

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 rm -fr build
 mkdir build
 cd build
-cmake .. -DCMAKE_PREFIX_PATH="/home/rquac004/.opt/rocm"
-make -n VERBOSE=1 -j 1
+#cmake .. -DCMAKE_C_COMPILER=/home/rquac004/.opt/rocm/llvm/bin/clang -DCMAKE_CXX_COMPILER=/home/rquac004/.opt/rocm/llvm/bin/clang++
+cmake .. -DCMAKE_CXX_COMPILER=/home/rquac004/.opt/rocm/bin/hipcc
+make VERBOSE=1 -j 1
