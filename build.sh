d7e6c176f3c109aa8494686a23441988aec5ad06
Ryferion <42130642+Ryferion@users.noreply.github.com>
2023-04-17 14:13:18 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 rm -fr build
 mkdir build
-
-cp matrix1.txt build
-cp matrix2.txt build
-cp matrix3.txt build
-
 cd build
 #cmake .. -DCMAKE_C_COMPILER=/home/rquac004/.opt/rocm/llvm/bin/clang -DCMAKE_CXX_COMPILER=/home/rquac004/.opt/rocm/llvm/bin/clang++
 cmake .. -DCMAKE_CXX_COMPILER=/home/rquac004/.opt/rocm/bin/hipcc
 make VERBOSE=1 -j 1
+
+cp matrix1.txt build/bin
+cp matrix2.txt build/bin
+cp matrix3.txt build/bin
