8e17dc24eaef04082d603359915aff5b781f3552
Ryferion <42130642+Ryferion@users.noreply.github.com>
2023-04-19 14:28:37 -0700

    Update CUMaskPower.cpp

--- CUMaskPower.cpp
+++ CUMaskPower.cpp
@@ -10,7 +10,7 @@
 
 #define __HIP_PLATFORM_HCC__
 #define DEVICE_NUM 0
-#define TILE_SIZE 16
+#define TILE_SIZE 2
 
 using namespace std;
 using namespace std::chrono;
@@ -316,12 +316,9 @@ void* hip(void *args)
     HIP_CHECK(hipMemcpyAsync(B_device, B_host, sizeof(float) * B_size, hipMemcpyHostToDevice, streamMemory));
 
     // set up block dim and thread dim
-    dim3 blocks(col / 1 + 1, row / 1 + 1, 1); // 3D dimensions of the grid of blocks
-    dim3 threads(2, 2, 1); // 3D dimensions of a block of threads
+    dim3 blocks(col / TILE_SIZE + 1, row / TILE_SIZE + 1, 1); // 3D dimensions of the grid of blocks
+    dim3 threads(TILE_SIZE, TILE_SIZE, 1); // 3D dimensions of a block of threads
 
-    // dim3 blocks(col / TILE_SIZE + 1, row / TILE_SIZE + 1, 1); // 3D dimensions of the grid of blocks
-    // dim3 threads(TILE_SIZE, TILE_SIZE, 1); // 3D dimensions of a block of threads
-    
     // // power thread launch right before kernel launch
     // pthread_t pthread_id2;
     // struct powerArgs *powerThreadBefore = (struct powerArgs *) malloc(sizeof(struct powerArgs));
